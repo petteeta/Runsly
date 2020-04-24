9d0160152cfc18b07e1a8ca00729f2bc846c58ea
sjpet <sjpettee@gmail.com>
2020-04-23 21:27:57 -0600

    finish race profile

--- app/src/main/java/com/glacierwebcreative/runsly/RaceProfileActivity.java
+++ app/src/main/java/com/glacierwebcreative/runsly/RaceProfileActivity.java
@@ -20,13 +20,26 @@ public class RaceProfileActivity extends AppCompatActivity {
         Race currentRace = (Race) getIntent().getSerializableExtra("CurrentRace");
         System.out.println(currentRace);
 
+
         TextView raceNameTextView = findViewById(R.id.race_name);
         raceNameTextView.setText(currentRace.getmRaceName());
+
+        TextView monthTextView = findViewById(R.id.month);
+        monthTextView.setText(currentRace.getmMonth());
+
+        TextView dayInMonth = findViewById(R.id.day_in_month);
+        dayInMonth.setText(String.valueOf(currentRace.getmDayInMonth()));
 
         TextView raceCityStateTextView = findViewById(R.id.city_and_state);
         raceCityStateTextView.setText(currentRace.getmCityAndState());
 
         TextView availableRacesTextView = findViewById(R.id.races_offered);
         availableRacesTextView.setText(currentRace.getmRacesOffered());
+
+        TextView synopsisTextView= findViewById(R.id.synopsis);
+        synopsisTextView.setText(currentRace.getmSynopsis());
+
+        TextView directorOrganizerTextView= findViewById(R.id.director_organizer);
+        directorOrganizerTextView.setText(currentRace.getmDirectorOrganizer());
     }
 }
