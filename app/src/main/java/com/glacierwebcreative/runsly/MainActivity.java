4b05e63519d0675efe8fc3cccd501ef60073fb3f
sjpet <sjpettee@gmail.com>
2020-04-29 22:04:18 -0600

    Go For It

--- app/src/main/java/com/glacierwebcreative/runsly/MainActivity.java
+++ app/src/main/java/com/glacierwebcreative/runsly/MainActivity.java
@@ -4,29 +4,42 @@ import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button racesButton;
+    private Button memberLoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Find the View that shows the Home Page
-        ImageView homePage = (ImageView) findViewById(R.id.home);
-        // this is change
-        // Set a click listener on that View
-        homePage.setOnClickListener(new View.OnClickListener() {
 
+        // "Races" button option
+        racesButton = findViewById(R.id.races_button);
+        racesButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 Intent chooseIntent = new Intent(MainActivity.this, ChooseRaceTypeActivity.class);
-
                 startActivity(chooseIntent);
             }
         });
+
+
+        // "Members Login" button option
+        memberLoginButton = findViewById(R.id.member_login_button);
+        memberLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent chooseIntent = new Intent(MainActivity.this, ChooseRaceTypeActivity.class);
+                startActivity(chooseIntent);
+            }
+        });
+
     }
 }
 
